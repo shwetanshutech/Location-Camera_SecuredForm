9573c8eedf0d06e21272ce2f372e53154d245f71
shwetanshutech <shwetanshu2000@gmail.com>
2021-05-28 15:55:16 +0530

    port from env variable

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const Datastore = require("nedb");
 const fetch = require("node-fetch");
 require("dotenv").config();
 const app = express();
-
-app.listen(3000, () => console.log("listening at 3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening at ${port}`));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
